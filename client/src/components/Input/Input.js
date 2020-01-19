cb21956339257f00fae8e1238faabf1fa54f2d4f
Saul Franco <saul.franco@tucanton.com>
2020-01-19 17:37:10 -0600

    Se actualiza error, si ya esta el usuario no lo deja escribri

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -5,14 +5,14 @@ import './Input.css';
 const Input = ({ setMessage, sendMessage, message }) => (
   <form className="form">
     <input
-      className="input"
-      type="text"
-      placeholder="Escribe un mensaje..."
-      value={message}
-      onChange={({ target: { value } }) => setMessage(value)}
-      onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+        className     ="input"
+        type          ="text"
+        placeholder   ="Escribe un mensaje..."
+        value       = { message}
+        onChange    = { (event) => setMessage(event.target.value) }
+        onKeyPress  = { event => event.key === 'Enter' ? sendMessage(event) : null }  
     />
-    <button className="sendButton" onClick={e => sendMessage(e)}>Enviar</button>
+    <button className="sendButton" onClick={(event) => sendMessage(event)}>Enviar</button>
   </form>
 )
 
